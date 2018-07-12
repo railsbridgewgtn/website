178542259a6477d561f4c6e0ea4dcd96bde4fcf6
heisenburger <serenadantingchen@gmail.com>
2018-07-13 08:45:49 +1200

    add nav

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Scroll listener for dimming heading
 
-var scrollAt = 0,
-    opaqueAt = 0,
-    transparentAt = 800;
+var scrollAt = 0, // current scroll state
+    opaqueAt = 0, // start opaque at this scroll state
+    transparentAt = 800; // fade it out by here
 var titles = document.getElementsByClassName('titles');
 
 window.addEventListener('scroll', function(e) {
+  // Get scroll state
   scrollAt = e.target.scrollingElement.scrollTop;
+
 });
